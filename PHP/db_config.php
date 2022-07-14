1a32372f8ad1dd41f0d50325e9d2a33c3aeb1626
Sazhelle GM <57726772+ChromeSkullex@users.noreply.github.com>
2022-07-14 17:53:32 -0400

    Finished Pokemon Database IMPORTANT
    NO PUNCTUATION FOR VARCHAR TYPES
    
    Farfetch'd caused some major issues, so make sure to remove the punctuation

--- PHP/db_config.php
+++ PHP/db_config.php
@@ -15,23 +15,60 @@
             $db_name = "`".str_replace("`","``",$db_name)."`";
             $link->query("CREATE DATABASE IF NOT EXISTS $db_name");
             $link->query("use $db_name");
-            /*
-            $sql = "CREATE TABLE IF NOT EXISTS poke_stat(
-            id INT NOT NULL PRIMARY KEY NOT NULL,
-            id_name VARCHAR(20),
-            display_name VARCHAR(20),
-            type_name JSON,
-            allowed_moveset JSON,
-            efficiency JSON,
-            base_stat JSON
-        
-            )";*/
             $sql = file_get_contents("db_ini.sql");
-        if ($link->exec($sql)){
-            echo "error";
-        }else {
-            echo "Created";
-        }
+            // Once initialized, populate tables
+            if ($link->exec($sql)){
+                echo "Server Error: Database not Initialized correctly";
+            }else {
+
+                $json_obj = file_get_contents("../RAWDATA/pokemon.json");
+                $json_poke = json_decode($json_obj, true);
+                foreach ($json_poke as $key=> $poke_name){
+
+                    $poke_name_id = $key;
+                    //echo ($json_a[$poke_name_id]['title']);
+                    $display_name = $json_poke[$poke_name_id]['title'];
+                    $img_link = $json_poke[$poke_name_id]['img'];
+                    $type_name =json_encode( $json_poke[$poke_name_id]['type']) ;
+                    $allowed_moveset = json_encode($json_poke[$poke_name_id]['efficiency']);
+                    $efficiency = json_encode($json_poke[$poke_name_id]['allowed_moves']);
+                    $base_stat = json_encode($json_poke[$poke_name_id]['base_stat']);
+                    echo "$display_name, $img_link    ";
+                    $sql_find = "SELECT COUNT(*) FROM poke_stat WHERE id_name='".$poke_name_id. "'";
+                    $result = $link->query($sql_find);
+                    $num_find = $result->fetch();
+                    print_r($num_find[0]);
+                    if ($num_find[0] < 1){
+                        $sql = "INSERT INTO poke_stat(id_name, display_name, img_link, type_name, allowed_moveset, efficiency, 
+                      base_stat) VALUES ('$poke_name_id', '$display_name', '$img_link', '$type_name', '$allowed_moveset', '$efficiency', '$base_stat')";
+                        $link->exec($sql);
+
+                        echo "Created<p>";
+                    }
+                    echo "<p>";
+                    /*
+                    $sql_find = "SELECT COUNT(*) FROM poke_stat WHERE id_name='".$poke_name_id. "'";
+                    $result = $link->query($sql_find);
+                    $num_find = $result->fetch();
+                    print_r($num_find[0]);
+
+                    $sql = "INSERT IGNORE INTO poke_stat(id_name, display_name, img_link, type_name, allowed_moveset, efficiency, 
+                      base_stat) VALUES ('$poke_name_id', '$display_name', '$img_link', '$type_name', '$allowed_moveset', '$efficiency', '$base_stat')";
+                    $link->exec($sql);
+
+                    echo "Created";*/
+                    /*
+                    if ($num_find[0] < 1){
+                        $sql = "INSERT IGNORE INTO poke_stat(id_name, display_name, img_link, type_name, allowed_moveset, efficiency, 
+                      base_stat) VALUES ('$poke_name_id', '$display_name', '$img_link', '$type_name', '$allowed_moveset', '$efficiency', '$base_stat')";
+                        $link->exec($sql);
+
+                        echo "Created";
+                    }*/
+                }
+
+
+            }
         }
         catch (PDOException $e){
 
--- PHP/db_ini.sql
+++ PHP/db_ini.sql
@@ -1,9 +1,34 @@
-CREATE TABLE IF NOT EXISTS poke_stat(
-    id INT NOT NULL PRIMARY KEY NOT NULL,
-    id_name VARCHAR(20),
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL PRIMARY KEY NOT NULL AUTO_INCREMENT,
     display_name VARCHAR(20),
+    username VARCHAR(20),
+    user_password VARCHAR(20),
+    active_pokemon JSON
+);
+CREATE TABLE IF NOT EXISTS poke_actor(
+    id INT NOT NULL PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_id INT,
+    pokemon_id INT,
+    self_stat JSON,
+    active_moveset JSON
+);
+CREATE TABLE IF NOT EXISTS poke_stat(
+    id INT NOT NULL PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_name VARCHAR(100),
+    display_name VARCHAR(100),
+    img_link VARCHAR(2083),
     type_name JSON,
     allowed_moveset JSON,
     efficiency JSON,
     base_stat JSON
-)
+);
+
+CREATE TABLE IF NOT EXISTS moveset(
+    id INT NOT NULL PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_name VARCHAR(20),
+    title VARCHAR(20),
+    type VARCHAR(20),
+    cat VARCHAR(20),
+    stats JSON,
+    effect VARCHAR(250)
+);
