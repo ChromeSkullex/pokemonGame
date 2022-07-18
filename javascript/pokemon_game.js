07325b5f427308ced17a585607fd27c21de6a1bc
Sazhelle GM <57726772+ChromeSkullex@users.noreply.github.com>
2022-07-18 16:01:46 -0400

    Reduce health and animation Test buttons done

--- javascript/pokemon_game.js
+++ javascript/pokemon_game.js
@@ -3,12 +3,49 @@ const IMG_HEIGHT = 150;
 const CANVAS_WIDTH = 720;
 const CANVAS_HEIGHT = 300;
 const IMG_SCALE = 192
+const PLAYER_ORI_POS = -2 * IMG_OFFSET;
+var poke_actor = 2; // Temp
+var poke_json;
+
+var poke_ene = 5;
+var poke_json_ene;
+
+var poke_health = 100;
+
+var buttonArr = [[100,415],[100+200+50,415],[100+200+50,415+35+10],[100,415+35+10]]
+var buttonText = ["Redu. HP","Animate","Ene HP","Game Condition"]
+
+var animation_f;
+var player_X = PLAYER_ORI_POS;
+var con_mov;
+
+var poke_actor_img = new Image();
+var poke_trainer_img = new Image();
 
 
 $(document).ready(function (){
    var c = document.getElementById("battle_game")
    var ctx = c.getContext('2d')
    ctx.imageSmoothingEnabled = false; // make it not blurry
+
+   //Getting pokemon for player
+   $.ajax({
+      url: '../PHP/get_methods_poke.php',
+      data: {"get_all_data": poke_actor},
+      async: false,
+      success(e){
+         poke_json = JSON.parse(e);
+      }
+   });
+   //Getting pokemon for ene
+   $.ajax({
+      url: '../PHP/get_methods_poke.php',
+      data: {"get_all_data": poke_ene},
+      async: false,
+      success(e){
+         poke_json_ene = JSON.parse(e);
+      }
+   });
 
    background_arr = ['island.png','mountain.png','plains.png'];
    var background_img = new Image();
@@ -21,62 +58,193 @@ $(document).ready(function (){
       load_UI(ctx)
    }
 
+   c.addEventListener('click', function (e){
+      var button_choice = button_handler(c, ctx, e)
+      if (button_choice === 0){
+         poke_health-=10;
+         dmg_take = 10;
+         requestAnimationFrame(reduce_hp_anim)
+      }
+      else if (button_choice=== 1){
+         animation_f = 30;
+         con_mov = true;
+         requestAnimationFrame(animate_user)
+      }
 
+      else if (button_choice === 3){
+
+      }
+
+   })
+   function create_win(){
+      
+   }
+   // Animation functions
+   function reduce_hp_anim(){
+      if (dmg_take>0){
+         poke_health--;
+         hp_UI(ctx)
+         requestAnimationFrame(reduce_hp_anim)
+
+      }
+      dmg_take--;
+   }
+
+   function animate_user(){
+      if (animation_f>0){
+         ctx.drawImage(background_img, 0, -90)
+         if (con_mov){
+            player_X -=30;
+         }
+         if (player_X < -420){
+            con_mov = false;
+         }
+         animate_user_load(ctx);
+         animate_trainer_load(ctx);
+         load_UI(ctx)
+         console.log("sad")
+         requestAnimationFrame(animate_user)
+
+      }
+      else {
+         ctx.drawImage(background_img, 0, -90)
+         player_X = PLAYER_ORI_POS;
+         animate_user_load(ctx);
+         animate_trainer_load(ctx);
+         load_UI(ctx)
+         console.log("sad")
+
+      }
+      animation_f--;
+   }
 
 });
 
-function load_UI(ctx){
+function animate_user_load(ctx){
+   ctx.translate(IMG_OFFSET, IMG_HEIGHT);
+   ctx.scale(-1, 1);
+   ctx.drawImage(poke_actor_img, player_X, IMG_HEIGHT - CANVAS_HEIGHT / 2, IMG_SCALE, IMG_SCALE)
+   ctx.setTransform(1, 0, 0, 1, 0, 0);
 }
 
-function load_player(ctx){
-   /*
-   var poke_arr = [];
-   for (var i = 0; i < 6;i++){
-      let x = Math.floor((Math.random()*151)+1);
-      poke_arr.push(x);
+function animate_trainer_load(ctx){
+   ctx.drawImage(poke_trainer_img, CANVAS_WIDTH-IMG_OFFSET-IMG_SCALE,IMG_HEIGHT, IMG_SCALE,IMG_SCALE)
+
+}
+
+function button_handler(c, ctx, e){
+   const canvas = c.getBoundingClientRect();
+   var mouseX = ((e.clientX - canvas.left) / (canvas.right - canvas.left)) * c.width;
+   var mouseY = ((e.clientY - canvas.top) / (canvas.bottom  - canvas.top)) * c.height;
+   console.log(mouseX, mouseY);
+
+   if(mouseX > 100 && mouseX < 300 && mouseY > 415 && mouseY<450){
+      console.log("1")
+      return 0;
+   }
+   else if (mouseX > 350 && mouseX < 550 && mouseY > 415 && mouseY<450){
+      console.log("2")
+      return 1;
+   }
+   else if(mouseX > 100 && mouseX < 300 && mouseY > 460 && mouseY<500){
+      console.log("3")
+      return 2;
+   }
+   else if (mouseX > 350 && mouseX < 550 && mouseY > 460 && mouseY<500){
+      console.log("4")
+      return 3;
    }
 
-   console.log(poke_arr);*/
 
-   $.ajax({
-      url: '../PHP/get_methods_poke.php',
-      data: {"get_img": "nidoran-f"},
-      async: false,
-      success(e){
-         console.log(e)
-         var poke_actor_img = new Image();
-         poke_actor_img.src = e;
-         poke_actor_img.onload = function (){
-            console.log(poke_actor_img)
-            ctx.translate(IMG_OFFSET,IMG_HEIGHT);
-            ctx.scale(-1,1);
-            ctx.drawImage(poke_actor_img,-2*IMG_OFFSET,IMG_HEIGHT-CANVAS_HEIGHT/2, IMG_SCALE,IMG_SCALE)
-            console.log(poke_actor_img)
-            ctx.setTransform(1,0,0,1,0,0);
+   return -1;
 
-         }
-      }
-   });
+}
+
+
+function load_UI(ctx){
+
+   ctx.fillStyle = "#ffbaae";
+   ctx.fillRect(0,400,CANVAS_WIDTH, 100)
+
+   ctx.fillStyle = '#F5F5F5FF';
+   ctx.fillRect(0,0, (1/3)*CANVAS_WIDTH, 50);
+
+   ctx.fillStyle = '#F5F5F5FF';
+   ctx.fillRect(CANVAS_WIDTH-(1/3)*CANVAS_WIDTH,0, (1/3)*CANVAS_WIDTH, 50);
+
+   hp_UI(ctx);
+   hp_UI_ene(ctx);
+   for (var i = 0 ; i < 4; i++){
+      buttons_UI(ctx, buttonArr[i][0],buttonArr[i][1], buttonText[i]);
+      buttons_UI(ctx, buttonArr[i][0],buttonArr[i][1], buttonText[i]);
+      buttons_UI(ctx, buttonArr[i][0],buttonArr[i][1], buttonText[i]);
+      buttons_UI(ctx, buttonArr[i][0],buttonArr[i][1], buttonText[i]);
+   }
+   text_UI(ctx, 10, 20, poke_json['display_name'])
+
+
+   //hp_UI_ene(ctx);
+
+}
+
+function buttons_UI(ctx, posX, posY, text){
+   ctx.fillStyle = "#382c2e";
+   butH = 35;
+   butW = 200;
+
+   ctx.fillRect(posX,posY,butW, butH)
+
+   ctx.font = '20px Arial';
+   ctx.fillStyle = '#e5dddd'
+   ctx.fillText(text, posX+50, posY+25);
+}
+
+
+function hp_UI_ene(ctx){
+   ctx.fillStyle = "#96d152";
+   var health_width = ((1/3)*CANVAS_WIDTH)*(4/5)
+   ctx.fillRect(CANVAS_WIDTH-health_width-40,25, health_width, 10);
+   text_UI(ctx, CANVAS_WIDTH-health_width-40, 20, poke_json_ene['display_name'])
+}
+
+
+function text_UI(ctx, posX, posY, display_name){
+   ctx.font = '20px Arial';
+   ctx.fillStyle = '#4b4646'
+   console.log(display_name)
+   ctx.fillText(display_name, posX, posY);
+}
+
+function hp_UI(ctx){
+   ctx.fillStyle = "#ffffff";
+   var health_width = ((1/3)*CANVAS_WIDTH)*(4/5)
+   ctx.fillRect(10,25, health_width, 10);
+   ctx.fillStyle = "#96d152";
+   ctx.fillRect(10,25, health_width*(poke_health/100), 10);
+
+}
+
+function load_player(ctx) {
+
+   poke_actor_img.src = poke_json['img_link'];
+   poke_actor_img.onload = function () {
+      ctx.translate(IMG_OFFSET, IMG_HEIGHT);
+      ctx.scale(-1, 1);
+      ctx.drawImage(poke_actor_img, player_X, IMG_HEIGHT - CANVAS_HEIGHT / 2, IMG_SCALE, IMG_SCALE)
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+   }
 }
 
 
 
 
 function load_trainer(ctx){
-   $.ajax({
-      url: '../PHP/get_methods_poke.php',
-      data: {"get_img": "bulbasaur"},
-      async: false,
-      success(e){
-         console.log(e)
-         var poke_actor_img = new Image();
-         poke_actor_img.src = e;
-         poke_actor_img.onload = function (){
-            ctx.drawImage(poke_actor_img, CANVAS_WIDTH-IMG_OFFSET-IMG_SCALE,IMG_HEIGHT, IMG_SCALE,IMG_SCALE)
-            console.log(poke_actor_img)
-         }
-      }
-   });
+   poke_trainer_img.src = poke_json_ene['img_link'];
+   poke_trainer_img.onload = function (){
+      ctx.drawImage(poke_trainer_img, CANVAS_WIDTH-IMG_OFFSET-IMG_SCALE,IMG_HEIGHT, IMG_SCALE,IMG_SCALE)
+      console.log(poke_trainer_img)
+   }
 }
 
 
